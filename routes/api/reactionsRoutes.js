ce56199d5e77565f269ddd9ceefbcff0160dc9ac
jjjgm <businessjgm@icloud.com>
2023-05-04 14:58:51 -0700

    reactions route, thought route

--- routes/api/reactionsRoutes.js
+++ routes/api/reactionsRoutes.js
@@ -1,8 +1,18 @@
 const router = require('express').Router();
 
+const {
+    createReaction,
+    deleteReaction
+} = require ('../../controllers/thought')
 
 // POST (w/ SINLGE THOUGHTS ARRAY FIELD)
-
 // DELETE REACTIONS (w/ reactionId)
+router.route('/:thoughtId').post(createReaction).delete(deleteReaction);
+
+//WILL POST THOUGHTS TO REACTIONS ARRAY - remember it's relational to a thought object
+router.route('/:thoughtId/reactions').post(createReaction);
+
+// ROUTE TO DELETE REACTIONS (w/ reactionId)- remember it's relational to a thought object
+router.route('/:thoughtId/reactions/:reactionId').delete(deleteReaction);
 
 module.exports = router;
--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -1,13 +1,25 @@
 const router = require('express').Router();
 
+const {
+    getThought,
+    getSingleThought,
+    createThought,
+    updateThought,
+    deleteThought,
+} = require ('../../controllers/thought');
+
+// ROUTES THESE (get & post) TO /api/thoughts
+router.route('/').get(getThought).post(createThought);
+
+// ROUTES THESE (get by id, out, and delete) TO /api/:thoughtId
+router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
+
 //GET ALL THOUGHTS
-
 //GET SINGLE THOUGHT BY ID
-
 //POST NEW THOUGHT (w/ _id)
+//PUT NEW THOUGHT (w/ _id)
+//DELETE THOUGHT (w/ _id)
 
-// PUT NEW THOUGHT (w/ _id)
-
-// DELETE THOUGHT (w/ _id)
+// may need to add reactions here as they are directly relational to thought objects
 
 module.exports = router;
