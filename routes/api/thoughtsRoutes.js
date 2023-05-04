1f15a15db4bec5b8ecaebd33e505c80443bceba7
jjjgm <businessjgm@icloud.com>
2023-05-04 16:26:44 -0700

    moved reactions into thought routes, need to create controllers)

--- routes/api/thoughtsRoutes.js
+++ routes/api/thoughtsRoutes.js
@@ -8,18 +8,27 @@ const {
     deleteThought,
 } = require ('../../controllers/thought');
 
+//REACTION SPECIFIC
+const {
+    createReaction,
+    deleteReaction
+} = require ('../../controllers/thought')
+
 // ROUTES THESE (get & post) TO /api/thoughts
 router.route('/').get(getThought).post(createThought);
 
-// ROUTES THESE (get by id, out, and delete) TO /api/:thoughtId
+// ROUTES THESE (get by id, put, and delete) TO /api/:thoughtId
 router.route('/:thoughtId').get(getSingleThought).put(updateThought).delete(deleteThought);
 
-//GET ALL THOUGHTS
-//GET SINGLE THOUGHT BY ID
-//POST NEW THOUGHT (w/ _id)
-//PUT NEW THOUGHT (w/ _id)
-//DELETE THOUGHT (w/ _id)
+// GET ALL THOUGHTS
+// GET SINGLE THOUGHT BY ID
+// POST NEW THOUGHT (w/ _id)
+// PUT NEW THOUGHT (w/ _id)
+// DELETE THOUGHT (w/ _id)
+// adding reactions here as they are directly relational to thought objects
+// POST  REACTION (w/ SINGLE THOUGHTS ARRAY FIELD)
+// DELETE REACTION  
 
-// may need to add reactions here as they are directly relational to thought objects
+router.route('/thoughts/:thoughtId/reactions').post(createReaction).delete(deleteReaction);
 
 module.exports = router;
--- routes/api/usersRoutes.js
+++ routes/api/usersRoutes.js
@@ -1,19 +1,31 @@
 const router = require('express').Router();
 
+const {
+    getUser,
+    getSingleUser,
+    createUser,
+    updateUser,
+    deleteUser,
+    createFriendship,
+    deleteFriendship,
+} = require('../../controllers/user');
+
 // GET ALL USERS
-
 // GET SINGLE USER (_id)
-
 // POST NEW USER
-
 // PUT NEW USER (_id)
-
 // DELETE USER (_id)
-
-// REMEMBER TO REMOVE ASSOCIATED THOUGHTS FROM USER
-
 // POST NEW FRIEND 
-
 // DELETE FRIEND FROM USER LIST
+// BONUS: REMEMBER TO REMOVE ASSOCIATED THOUGHTS FROM USER
+
+// ROUTES THESE (get & post) TO /api/users
+router.route('/').get(getUser).post(createUser);
+
+// ROUTES THESE (gets a user by id, put, and delete) TO /api/:userId
+router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
+
+// ROUTES THESE (gets a user by id, put, and delete) TO /api/:userId/friends/:friendId
+router.route('/:userId/friends/:friendId').post(createFriendship).delete(deleteFriendship);
 
 module.exports = router;
